2681f6da3622fc46a26837c38c397120cabc45f2
nov matake <nov@matake.jp>
2011-03-02 19:09:12 +0900

    added missing files

---
+++ lib/rack/oauth2/server/authorize/error.rb
@@ -0,0 +1,61 @@
+module Rack
+  module OAuth2
+    module Server
+      module Authorize
+        class Error
+
+          DEFAULT_DESCRIPTION = {
+            :invalid_request => "The request is missing a required parameter, includes an unsupported parameter or parameter value, or is otherwise malformed.",
+            :invalid_client => "The client identifier provided is invalid.",
+            :unauthorized_client => "The client is not authorized to use the requested response type.",
+            :redirect_uri_mismatch => "The redirection URI provided does not match a pre-registered value.",
+            :access_denied => "The end-user or authorization server denied the request.",
+            :unsupported_response_type => "The requested response type is not supported by the authorization server.",
+            :invalid_scope => "The requested scope is invalid, unknown, or malformed."
+          }
+
+          def error!(error, description = nil, options = {})
+            description ||= DEFAULT_DESCRIPTION[error]
+            raise BadRequest.new(error, description, options.merge(:state => state, :redirect_uri => redirect_uri))
+          end
+
+          def invalid_request!(description = nil, options = {})
+            error!(:invalid_request, description, options)
+          end
+
+          def invalid_client!(description = nil, options = {})
+            error!(:invalid_client, description, options)
+          end
+
+          def unauthorized_client!(description = nil, options = {})
+            error!(:unauthorized_client, description, options)
+          end
+
+          def redirect_uri_mismatch!(description = nil, options = {})
+            no_redirect!
+            error!(:redirect_uri_mismatch, description, options)
+          end
+
+          def access_denied!(description = nil, options = {})
+            error!(:access_denied, description, options)
+          end
+
+          def unsupported_response_type!(description = nil, options = {})
+            error!(:unsupported_response_type, description, options)
+          end
+
+          def invalid_scope!(description = nil, options = {})
+            error!(:invalid_scope, description, options)
+          end
+
+          private
+
+          def no_redirect!
+            self.redirect_uri = nil
+          end
+
+        end
+      end
+    end
+  end
+end
---
+++ lib/rack/oauth2/server/resource/bearer.rb
@@ -0,0 +1,78 @@
+require 'rack/auth/abstract/request'
+
+module Rack
+  module OAuth2
+    module Server
+      module Resource
+        class Bearer < Abstract::Handler
+
+          ACCESS_TOKEN = 'rack.oauth2.bearer.oauth_token'
+
+          def initialize(app, &authenticator)
+            @app = app
+            super(&authenticator)
+          end
+
+          def call(env)
+            request = Request.new(env)
+            if request.oauth2?
+              authenticate!(request)
+              env[ACCESS_TOKEN] = request.access_token
+            end
+            @app.call(env)
+          rescue Error => e
+            e.finish
+          end
+
+          private
+
+          def authenticate!(request)
+            @authenticator.call(request)
+          end
+
+          class Request < Rack::Request
+            include Error::Resource
+
+            def initialize(env)
+              @env = env
+              @auth_header = Rack::Auth::AbstractRequest.new(env)
+            end
+
+            def oauth2?
+              access_token.present?
+            end
+
+            def access_token
+              tokens = [access_token_in_haeder, access_token_in_payload].compact
+              case Array(tokens).size
+              when 0
+                nil
+              when 1
+                tokens.first
+              else
+                invalid_request!('Both Authorization header and payload includes oauth_token.')
+              end
+            end
+
+            def access_token_in_haeder
+              if @auth_header.provided? && @auth_header.scheme == :oauth2
+                @auth_header.params
+              else
+                nil
+              end
+            end
+
+            def access_token_in_payload
+              if params['oauth_token'] && !params['oauth_signature_method']
+                params['oauth_token']
+              else
+                nil # This is OAuth1 request
+              end
+            end
+
+          end
+        end
+      end
+    end
+  end
+end
