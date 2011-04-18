df7a087b9aab88b053c7f64bd897608fb22b6968
nov matake <nov@matake.jp>
2011-04-19 02:41:32 +0900

    body_hash and signature class

---
+++ lib/rack/oauth2/access_token/mac/body_hash.rb
@@ -0,0 +1,10 @@
+module Rack
+  module OAuth2
+    class AccessToken
+      class MAC
+        class BodyHash
+        end
+      end
+    end
+  end
+end
---
+++ lib/rack/oauth2/access_token/mac/signature.rb
@@ -0,0 +1,10 @@
+module Rack
+  module OAuth2
+    class AccessToken
+      class MAC
+        class Signature
+        end
+      end
+    end
+  end
+end
