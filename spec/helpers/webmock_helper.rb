131b0c7f5ae06cfe377eddea6905f1e98418738d
nov matake <nov@matake.jp>
2011-05-18 22:47:20 +0900

    add missing files

---
+++ spec/helpers/webmock_helper.rb
@@ -0,0 +1,42 @@
+require 'webmock/rspec'
+
+module WebMockHelper
+  def mock_response(method, endpoint, response_file, options = {})
+    stub_request(method, endpoint).with(
+      request_for(method, options)
+    ).to_return(
+      response_for(response_file, options)
+    )
+  end
+
+  private
+
+  def request_for(method, options = {})
+    request = {}
+    if options[:access_token]
+      options[:params] ||= {}
+      options[:params][:oauth_token] = options[:access_token].to_s
+    end
+    if options[:params]
+      case method
+      when :post, :put
+        request[:body] = options[:params]
+      else
+        request[:query] = options[:params]
+      end
+    end
+    request
+  end
+
+  def response_for(response_file, options = {})
+    response = {}
+    response[:body] = File.new(File.join(File.dirname(__FILE__), '../mock_response', response_file))
+    if options[:status]
+      response[:status] = options[:status]
+    end
+    response
+  end
+end
+
+include WebMockHelper
+WebMock.disable_net_connect!
