1d4608accfa6c7a5db3bd394f2ac022c482deb36
nov matake <nov@matake.jp>
2012-08-07 23:07:05 +0900

    test case for issue #20

--- spec/rack/oauth2/oauth2_spec.rb
+++ spec/rack/oauth2/oauth2_spec.rb
@@ -28,4 +28,18 @@ describe Rack::OAuth2 do
       Rack::OAuth2.debugging?.should be_true
     end
   end
+
+  describe '.http_config' do
+    context 'when request_filter added' do
+      context 'when "debug!" is called' do
+        it 'should put Debugger::RequestFilter at last' do
+          Rack::OAuth2.debug!
+          Rack::OAuth2.http_config do |config|
+            config.request_filter << Proc.new {}
+          end
+          Rack::OAuth2.http_client.request_filter.last.should be_instance_of Rack::OAuth2::Debugger::RequestFilter
+        end
+      end
+    end
+  end
 end
