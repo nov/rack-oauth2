f465ec25cb0183f65d685cd8f6d20d9dfe1a2236
nov <nov@matake.jp>
2013-01-16 11:04:34 +0900

    spec (& bundle) update for debugger change

--- spec/rack/oauth2/debugger/request_filter_spec.rb
+++ spec/rack/oauth2/debugger/request_filter_spec.rb
@@ -8,21 +8,25 @@ describe Rack::OAuth2::Debugger::RequestFilter do
 
   describe '#filter_request' do
     it 'should log request' do
-      Rack::OAuth2.logger.should_receive(:info).with(
-        "======= [Rack::OAuth2] HTTP REQUEST STARTED =======\n" +
+      [
+        "======= [Rack::OAuth2] HTTP REQUEST STARTED =======",
         request.dump
-      )
+      ].each do |output|
+        Rack::OAuth2.logger.should_receive(:info).with output
+      end
       request_filter.filter_request(request)
     end
   end
 
   describe '#filter_response' do
     it 'should log response' do
-      Rack::OAuth2.logger.should_receive(:info).with(
-        "--------------------------------------------------\n" +
-        response.dump +
-        "\n======= [Rack::OAuth2] HTTP REQUEST FINISHED ======="
-      )
+      [
+        "--------------------------------------------------",
+        response.dump,
+        "======= [Rack::OAuth2] HTTP REQUEST FINISHED ======="
+      ].each do |output|
+        Rack::OAuth2.logger.should_receive(:info).with output
+      end
       request_filter.filter_response(request, response)
     end
   end
