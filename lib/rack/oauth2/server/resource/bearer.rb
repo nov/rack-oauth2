ee5e31174d4e9806399e6acd6bd550a710e02711
Jack Chu <kamuigt@gmail.com>
2013-02-07 03:38:24 -0500

    add support for rack 1.5.x

--- lib/rack/oauth2/server/resource/bearer.rb
+++ lib/rack/oauth2/server/resource/bearer.rb
@@ -27,7 +27,7 @@ module Rack
             end
 
             def access_token_in_header
-              if @auth_header.provided? && @auth_header.scheme == :bearer
+              if @auth_header.provided? && @auth_header.scheme.to_s == 'bearer'
                 @auth_header.params
               else
                 nil
